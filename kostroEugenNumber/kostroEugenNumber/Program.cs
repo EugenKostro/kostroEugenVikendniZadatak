e3792d7db9d4f65e3e68d6e93df04471ce1607b8
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2019-12-05 21:47:52 +0100

    Add files via upload

---
+++ kostroEugenNumber/kostroEugenNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;  
+
+
+namespace kostroEugenNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95F;
+            var character = 'A';
+            var firstName = "Mosh";
+            var isWorking = true;
+            var isWorking1 = false;
+            
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+            Console.WriteLine(isWorking1);
+        }
+    }
+}
---
+++ kostroEugenNumberPart2/kostroEugenNumberPart2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace kostroEugenNumberPart2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, byte.MaxValue);
+
+            const float Pi = 3.14f;
+            Pi = 1;
+        }
+    }
+}
